57455cea3096324f4cb0cc1faaa7e82149c4340e
Will Kaczmarek <kaczmarekwill@gmail.com>
2018-11-27 13:58:06 -0500

    Model corrections

--- leafs.py
+++ leafs.py
@@ -46,6 +46,8 @@ model.compile(loss='categorical_crossentropy',
             optimizer='rmsprop',
             metrics=['accuracy'])
 
+model.load_weights('my_model.h5')
+model.save('my_model.h5')
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
         rescale=1. / 255,
--- runsamples.py
+++ runsamples.py
@@ -5,20 +5,17 @@ from glob import glob
 
 class_names = glob("data/train/*") # Reads all the folders in which images are present
 class_names = sorted(class_names)
-name_id_map = dict(zip(class_names, range(len(class_names))))
+name_id_map = dict(zip(range(len(class_names)), class_names))
 
 model = load_model('my_model.h5')
 
 model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+                      optimizer='rmsprop',
+                                    metrics=['accuracy'])
 
 img = cv2.imread('test.jpg')
 img = cv2.resize(img, (64, 64))
 img = np.expand_dims(img, axis=0)
 classes = model.predict(img)
-
-for name, num in name_id_map.items():    # for name, age in dictionary.iteritems():  (for Python 2.x)
-    if 1 == classes[0][num]:
-        print(name)
-
+pred = classes.argmax(axis=-1)[0]
+print(name_id_map.get(pred).split('/')[-1].replace('_', ' ').title())
